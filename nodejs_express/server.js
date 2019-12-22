28f72ed25698d158027aba24e794793d8d0dfaf2
DaPawa <dapawa@protonmail.com>
2019-12-22 20:47:20 +0000

    actually added stuff to the repo
    yay code

--- nodejs_express/server.js
+++ nodejs_express/server.js
@@ -1,9 +1,33 @@
 'use strict';
-const http = require('http');
 const port = process.env.PORT || 1337;
-const express = 
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+const fs = require('fs');
+const express = require('express');
+const sql = require('mysql');
+
+const app = express();
+
+const connection = sql.createConnection({
+	host: 'localhost',
+	user: 'server',
+	database: 'gnglish'
+}).connect((err) => {
+	if (err) throw err;
+	console.log('Connected!');
+})
+
+app.get('/', (req, res) => {
+	fs.readFile('html/index.html', (err, data) => {
+		if (err) throw err;
+		res.status(200);
+		res.header('Content-Type', 'text/html');
+		res.write(data);
+		res.end();
+	});
+});
+
+app.get('/words', (req, res) => {
+
+})
+
+app.listen(port);
