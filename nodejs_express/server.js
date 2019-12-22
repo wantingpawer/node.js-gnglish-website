54d400582755ceeb5330428c757b21b4acb28607
DaPawa <dapawa@protonmail.com>
2019-12-22 12:48:48 +0000

    Add project files.

---
+++ nodejs_express/server.js
@@ -0,0 +1,9 @@
+'use strict';
+const http = require('http');
+const port = process.env.PORT || 1337;
+const express = 
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
